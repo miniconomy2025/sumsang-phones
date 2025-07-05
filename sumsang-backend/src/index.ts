3ca3b38ea552b873da47bc20952e4c24f8ae1127
Shashin Gounden <shashin.gounden@bbd.co.za>
2025-07-05 16:04:07 +0200

    finance endpoint

--- sumsang-backend/src/index.ts
+++ sumsang-backend/src/index.ts
@@ -1,10 +1,12 @@
 import express, {Request, Response} from 'express';
 import StockRoutes from './routes/StockRoutes.js';
+import DashboardRoutes from './routes/DashboardRoutes.js';
 
 const app = express();
 const PORT = 5000;
 
 app.use('/public-api/', StockRoutes);
+app.use('/public-api/', DashboardRoutes);
 
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`)
