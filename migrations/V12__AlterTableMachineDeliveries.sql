eeb4dc0128af6163c2c3a313863420f69ed6b8d4
Vusi Pekane <vusumuzi@bbd.co.za>
2025-07-06 15:39:03 +0200

    added new machine purchases table and altered machine deliveries table

---
+++ migrations/V12__AlterTableMachineDeliveries.sql
@@ -0,0 +1,22 @@
+CREATE TABLE machine_purchases (
+    machine_purchases_id SERIAL PRIMARY KEY,
+    phone_id INT NOT NULL REFERENCES phones(phone_id),
+    machines_purchased INT NOT NULL,
+    total_cost DECIMAL NOT NULL,
+    weight_per_machine INT NOT NULL,
+    rate_per_day INT NOT NULL
+);
+
+
+ALTER TABLE machine_deliveries
+ADD COLUMN total_units INT NOT NULL;
+
+ALTER TABLE machine_deliveries
+DROP COLUMN machine_id;
+
+ALTER TABLE machine_deliveries
+ADD COLUMN machine_purchases_id INT;
+
+ALTER TABLE machine_deliveries
+ADD CONSTRAINT fk_machine_deliveries_machine_purchases
+FOREIGN KEY (machine_purchases_id) REFERENCES machine_purchases(machine_purchases_id);
