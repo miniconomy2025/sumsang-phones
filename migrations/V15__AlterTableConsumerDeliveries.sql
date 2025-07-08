2e8f0cbf7efc0dbef9eb2a7959873fc0cde45d07
RonJ <Ron.Joseph@bbd.co.za>
2025-07-09 00:10:34 +0200

    Changed migration to string

--- migrations/V15__AlterTableConsumerDeliveries.sql
+++ migrations/V15__AlterTableConsumerDeliveries.sql
@@ -1,3 +1,3 @@
 ALTER TABLE consumer_deliveries DROP CONSTRAINT consumer_deliveries_delivery_reference_key;
-ALTER TABLE consumer_deliveries ALTER COLUMN delivery_reference TYPE UUID USING (uuid_generate_v4());
+ALTER TABLE consumer_deliveries ALTER COLUMN delivery_reference TYPE VARCHAR(20);
 ALTER TABLE consumer_deliveries ADD CONSTRAINT uq_consumer_deliveries_delivery_reference UNIQUE (delivery_reference);
