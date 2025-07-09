b607844f404e2d49c7863ae6af3c649b102b9591
Gregory Conroy <gregory.conroy@bbd.co.za>
2025-07-09 16:52:38 +0200

    Updated constant casing

--- sumsang-backend/src/constants/SystemSettingKeys.ts
+++ sumsang-backend/src/constants/SystemSettingKeys.ts
@@ -1,6 +1,6 @@
 export const systemSettingKeys = {
-	startEpoch: 'startEpoch',
-	currentDay: 'currentDay',
-	accountNumber: 'accountNumber',
-	loanNumber: 'loanNumber',
+	startEpoch: 'start_epoch',
+	currentDay: 'current_day',
+	accountNumber: 'account_number',
+	loanNumber: 'loan_number',
 };
