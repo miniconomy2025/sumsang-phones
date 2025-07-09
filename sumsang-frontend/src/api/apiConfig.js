cd9e99747836d511fac7447087e937f9b3161ffb
Shashin Gounden <shashin.gounden@bbd.co.za>
2025-07-09 22:12:09 +0200

    Pipeline

--- sumsang-frontend/src/api/apiConfig.js
+++ sumsang-frontend/src/api/apiConfig.js
@@ -2,6 +2,6 @@ export const USE_DUMMY_DATA = false;
 const isProduction = process.env.NODE_ENV === 'production';
 
 export const BASE_URL = isProduction
-	? 'https://sumsang-phones-api.projects.bbdgrad.com/internal-api'
+	? 'https://sumsang-phones-api.projects.bbdgrad.com:444/internal-api'
 	: 'http://localhost:3000/internal-api';
 
