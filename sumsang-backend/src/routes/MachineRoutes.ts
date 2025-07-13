271bd84235c9633c8ae064f9977819ab7e559b1b
Shashin Gounden <shashin.gounden@bbd.co.za>
2025-07-13 12:13:52 +0200

    OU Options

--- sumsang-backend/src/routes/MachineRoutes.ts
+++ sumsang-backend/src/routes/MachineRoutes.ts
@@ -1,8 +1,9 @@
 import { Router } from 'express';
 import { MachineController } from '../controllers/MachineController.js';
 import { verifyOU } from '../middleware/verifyOU.js';
+import { OrganizationalUnit } from '../types/OrganizationalUnitOptions.js';
 const router = Router();
 
-router.post('/machine-failure', verifyOU('thoh'), MachineController.breakMachine);
+router.post('/machine-failure', verifyOU(OrganizationalUnit.thoh), MachineController.breakMachine);
 
 export default router;
